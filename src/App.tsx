d4083d9326723df3f26cca4384eaded2afdfb704
Eien <depedronico02@gmail.com>
2025-11-10 15:34:17 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './main.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className='text-4xl'>Countdown Timer</h1>
+
+
+      </div>
+    </>
+  )
+}
+
+export default App
